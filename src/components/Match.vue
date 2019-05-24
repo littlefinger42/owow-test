a6c21b56368c0bfd1fb2b01dbf647907622a635f
littlefinger42 <lauriemail@protonmail.com>
2019-05-24 11:15:19 +0200

    Created Match component, which is looped in the Round component. This is looped based on the Round component's Match array in its local Data object

---
+++ src/components/Match.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{users}}</p>
+    <p>{{status}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Match",
+  props: {
+    users: Object,
+    status: Number
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
